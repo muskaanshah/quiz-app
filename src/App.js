a6862b11946a03c6cc304269d115986e637d47c8
muskaanshah <hmuskaan11@gmail.com>
2022-04-27 15:56:38 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import tw from "tailwind-styled-components";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App text-red-300">
+            <HEADER>New</HEADER>
+        </div>
+    );
 }
+
+const HEADER = tw.h1`underline text-2xl`;
 
 export default App;
