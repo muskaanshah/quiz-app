666d9e4074c761958b22e27627cabe72750e43f5
muskaanshah <hmuskaan11@gmail.com>
2022-04-29 18:18:00 +0530

    fix: type

--- src/services/authServices.ts
+++ src/services/authServices.ts
@@ -7,23 +7,28 @@ import {
 
 const googleProvider = new GoogleAuthProvider();
 
-const userLogin = async (auth: any, email:string, password:string, isGoogleSignIn = false) => {
+const userLogin = async (
+    auth: any,
+    email: string,
+    password: string,
+    isGoogleSignIn: boolean = false
+) => {
     try {
         const res = isGoogleSignIn
             ? await signInWithPopup(auth, googleProvider)
             : await signInWithEmailAndPassword(auth, email, password);
         console.log(res.user);
-    } catch (err: any) {
-        console.log(err.message);
+    } catch (error: any) {
+        console.log(error.message);
     }
 };
 
-const userSignup = async (auth:any, email:string, password:string) => {
+const userSignup = async (auth: any, email: string, password: string) => {
     try {
         const res = await createUserWithEmailAndPassword(auth, email, password);
         console.log(res.user);
-    } catch (err: any) {
-        console.log(err.message);
+    } catch (error: any) {
+        console.log(error.message);
     }
 };
 
